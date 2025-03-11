7009d95938704975cb7286b10da8a610d639a5b5
Ivgenium <genrymorgon@mail.ru>
2025-03-12 00:06:19 +0300

    1. Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/set/SimpleArraySet.java
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -14,24 +14,30 @@ public class SimpleArraySet<T> implements SimpleSet<T> {
 
     @Override
     public boolean add(T value) {
+        boolean rsl = true;
         for (T t : set) {
             if (Objects.equals(t, value)) {
-                return false;
+                rsl = false;
+                break;
             }
         }
-        set.add(value);
-        modCount++;
-        return true;
+        if (rsl) {
+            set.add(value);
+            modCount++;
+        }
+        return rsl;
     }
 
     @Override
     public boolean contains(T value) {
+        boolean rsl = false;
         for (T t : set) {
             if (Objects.equals(t, value)) {
-                return true;
+                rsl = true;
+                break;
             }
         }
-        return false;
+        return rsl;
     }
 
     @Override
