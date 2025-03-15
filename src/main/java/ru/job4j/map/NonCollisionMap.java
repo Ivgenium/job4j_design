00cdcc61fc7e20615557fd25390fc4564dff3d8a
Ivgenium <genrymorgon@mail.ru>
2025-03-15 16:57:27 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -0,0 +1,134 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+    private int count = 0;
+    private int modCount = 0;
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        boolean rsl = false;
+        int index = indexFor(hash(Objects.hashCode(key)));
+        if (table[index] == null) {
+            count++;
+            modCount++;
+            if (count == (int) (capacity * LOAD_FACTOR)) {
+                expand();
+                index = indexFor(hash(Objects.hashCode(key)));
+            }
+            table[index] = new MapEntry<>(key, value);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public V get(K key) {
+        V rsl = null;
+        int index = indexFor(hash(Objects.hashCode(key)));
+        MapEntry<K, V> p = table[index];
+        if (p != null) {
+            if (Objects.hashCode(p.key) == Objects.hashCode(key)
+                    && Objects.equals(p.key, key)) {
+                rsl = p.value;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        boolean rsl = false;
+        int index = indexFor(hash(Objects.hashCode(key)));
+        MapEntry<K, V> p = table[index];
+        if (p != null) {
+            if (Objects.hashCode(p.key) == Objects.hashCode(key)
+                    && Objects.equals(p.key, key)) {
+                p.key = null;
+                p.value = null;
+                p = null;
+                table[index] = null;
+                rsl = true;
+                count--;
+                modCount++;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private int point = 0;
+            private int index = 0;
+            private final int expectedModCount = NonCollisionMap.this.modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (NonCollisionMap.this.modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return point < count;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                while (table[index] == null) {
+                    index++;
+                }
+                point++;
+                return table[index++].key;
+            }
+        };
+    }
+
+    private int hash(int hashCode) {
+        return (hashCode == 0) ? 0 : (hashCode) ^ (hashCode >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (capacity - 1);
+    }
+
+    private void expand() {
+        capacity = capacity * 2;
+        MapEntry<K, V>[] newTable = new MapEntry[capacity];
+        for (MapEntry<K, V> element : table) {
+            if (element != null) {
+                newTable[indexFor(hash(Objects.hashCode(element.key)))] = element;
+            }
+        }
+        table = newTable;
+    }
+
+    private static class MapEntry<K, V> {
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        NonCollisionMap<Integer, Integer> map = new NonCollisionMap<>();
+        System.out.println(map.hash(0));
+        System.out.println(map.hash(65535));
+        System.out.println(map.hash(65536));
+        System.out.println(map.indexFor(0));
+        System.out.println(map.indexFor(7));
+        System.out.println(map.indexFor(8));
+    }
+}
