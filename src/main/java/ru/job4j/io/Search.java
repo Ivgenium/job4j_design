e65975a39d33d2bf9cd4605d0c10e7b5edc31882
Ivgenium <genrymorgon@mail.ru>
2025-04-21 20:24:53 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        checkArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void checkArgs(String[] args) {
+        String ln = System.lineSeparator();
+        if (args.length != 2) {
+            throw new IllegalArgumentException("There should be two args:" + ln
+                    + "The first parameter is the initial folder." + ln
+                    + "The second parameter is the file extension to be searched for.");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException(String.format("The initial folder: %s  does not exist.", args[0]));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("The file extension must start with \".\"");
+        }
     }
 }
