4bbe6c2210782f98b8b9212e2b25b79d4acd5370
Ivgenium <genrymorgon@mail.ru>
2025-07-13 14:53:58 +0300

    2. Группировка и агрегатные функции [#373618]

--- db/devices_people.sql
+++ db/devices_people.sql
@@ -1,46 +1,46 @@
--- create table devices
--- (
---     id    serial primary key,
---     name  varchar(255),
---     price float
--- );
+create table devices
+(
+    id    serial primary key,
+    name  varchar(255),
+    price float
+);
 
--- create table people
--- (
---     id   serial primary key,
---     name varchar(255)
--- );
+create table people
+(
+    id   serial primary key,
+    name varchar(255)
+);
 
--- create table devices_people
--- (
---     id        serial primary key,
---     device_id int references devices (id),
---     people_id int references people (id)
--- );
+create table devices_people
+(
+    id        serial primary key,
+    device_id int references devices (id),
+    people_id int references people (id)
+);
 
--- insert into people(name) values ('Вовик'), ('Васёк'), ('Ванёк');
+insert into people(name) values ('Вовик'), ('Васёк'), ('Ванёк');
 
--- insert into devices(name, price) values
--- 	('Мобильный телефон1', 1500.00),
--- 	('Мобильный телефон2', 2000.00),
--- 	('Мобильный телефон3', 2500.00),
--- 	('Ноутбук1', 5000.00),
--- 	('Ноутбук2', 6000.00),
--- 	('Ноутбук3', 7000.00),
--- 	('Телевизор1', 4500.00),
--- 	('Телевизор2', 5500.00),
--- 	('Телевизор3', 6500.00);
+insert into devices(name, price) values
+	('Мобильный телефон1', 1500.00),
+	('Мобильный телефон2', 2000.00),
+	('Мобильный телефон3', 2500.00),
+	('Ноутбук1', 5000.00),
+	('Ноутбук2', 6000.00),
+	('Ноутбук3', 7000.00),
+	('Телевизор1', 4500.00),
+	('Телевизор2', 5500.00),
+	('Телевизор3', 6500.00);
 	
--- insert into devices_people(device_id, people_id) values
--- 	(1, 1),
--- 	(2, 2),
--- 	(3, 3),
--- 	(4, 1),
--- 	(5, 2),
--- 	(6, 3),
--- 	(7, 1),
--- 	(8, 2),
--- 	(9, 3);
+insert into devices_people(device_id, people_id) values
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 1),
+	(5, 2),
+	(6, 3),
+	(7, 1),
+	(8, 2),
+	(9, 3);
 
 select avg(d.price) as "Средняя цена устройств"
 from devices as d;
